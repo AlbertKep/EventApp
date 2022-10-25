3e608bf2489e8231c4837912adfc6266e5f14d60
Albert <albertkepski3@gmail.com>
2022-10-25 19:23:59 +0200

    style: add styles in  AddEvent page

--- src/pages/addEvent/AddEvent.js
+++ src/pages/addEvent/AddEvent.js
@@ -1,3 +1,84 @@
+// styles
+import { Button } from "../../components/button/Button.styled";
+import {
+  Container,
+  ColumnController,
+  AddEventForm,
+  InputContainer,
+  AddEventInputContainer,
+  ButtonContainer,
+} from "./AddEvent.styled";
+
+import { useState } from "react";
+
+const buttonStyles = {
+  color: "#3137e7",
+  borderColor: "#3137e7",
+  padding: "0.5em 1em",
+};
+
 export default function AddEvent() {
-  return <div>AddEvent</div>;
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <Container>
+      <AddEventForm>
+        <ColumnController>
+          <InputContainer>
+            <input
+              type="text"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+            />
+            <label htmlFor="name">Name</label>
+          </InputContainer>
+
+          <InputContainer>
+            <input type="text" id="date" />
+            <label htmlFor="date">Date</label>
+          </InputContainer>
+
+          <InputContainer>
+            <input
+              type="text"
+              id="location"
+              onChange={(e) => setLocation(e.target.value)}
+              value={location}
+              required
+            />
+            <label htmlFor="name">Location</label>
+          </InputContainer>
+        </ColumnController>
+
+        <ColumnController>
+          <AddEventInputContainer>
+            <label htmlFor="image">
+              <input type="file" id="image" />+ Add image
+            </label>
+          </AddEventInputContainer>
+
+          <InputContainer>
+            <textarea
+              name="description"
+              id="description"
+              cols="20"
+              rows="10"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+            ></textarea>
+            <label htmlFor="description"> Description</label>
+          </InputContainer>
+
+          <ButtonContainer>
+            <Button buttonStyles={buttonStyles}>Add Event</Button>
+          </ButtonContainer>
+        </ColumnController>
+      </AddEventForm>
+    </Container>
+  );
 }
---
+++ src/pages/addEvent/AddEvent.styled.js
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    height: 90vh;
+  }
+`;
+
+export const AddEventForm = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const ColumnController = styled.div`
+  flex-basis: 80%;
+  padding: 1em;
+
+  @media screen and (min-width: 768px) {
+    flex-basis: 35%;
+    margin: 1em 2em;
+  }
+`;
+
+export const InputContainer = styled.div`
+  margin: 3em 0;
+  position: relative;
+  color: #fff;
+
+  label {
+    margin-right: 1.3em;
+    position: absolute;
+    top: 0;
+    left: 0;
+    font-weight: 700;
+    transition: all 0.2s ease-in-out;
+
+    @media screen and (min-width: 768px) {
+      font-size: 1.3rem;
+    }
+  }
+
+  input {
+    outline: none;
+    width: 100%;
+    color: #fff;
+    border-bottom: 2px solid #fff;
+    padding: 0.5em;
+    font-size: 1.3rem;
+
+    &:focus {
+      border-color: #3137e7;
+    }
+
+    &:required:focus + label,
+    &:required:valid + label {
+      border-color: #3137e7;
+      color: #3137e7;
+      font-size: 1rem;
+      top: -30px;
+    }
+  }
+
+  textarea {
+    margin-top: 1em;
+    font-size: 1.3rem;
+    resize: none;
+    outline: none;
+    width: 100%;
+    color: #fff;
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid #fff;
+
+    &:focus {
+      border-color: #3137e7;
+    }
+
+    &:required:focus + label,
+    &:required:valid + label {
+      margin-top: 1em;
+      color: #3137e7;
+      font-size: 1rem;
+      top: -30px;
+    }
+  }
+`;
+
+export const AddEventInputContainer = styled(InputContainer)`
+  margin-bottom: 5em;
+  label {
+    cursor: pointer;
+    top: -30px;
+    display: inline-block;
+
+    &:hover {
+      color: #3137e7;
+    }
+  }
+  input {
+    display: none;
+  }
+`;
+
+export const ButtonContainer = styled(InputContainer)`
+  text-align: right;
+`;
