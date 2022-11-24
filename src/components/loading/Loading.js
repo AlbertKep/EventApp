ed4cd8fb384044c118955f4b6860674e75967ae0
Albert <albertkepski3@gmail.com>
2022-11-24 19:05:56 +0100

    feat: add dots animation

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,12 +1,44 @@
+import { motion } from "framer-motion";
+
 import { LoadingContainer, Dots, Dot, Text } from "./Loading.styled";
+
+const dotsContainer = {
+  initial: { scale: 0 },
+  animate: {
+    scale: 1,
+    transition: {
+      staggerChildren: 0.35,
+    },
+  },
+};
+
+const dotsVariants = {
+  initial: {
+    scale: 0,
+  },
+  animate: {
+    scale: 1,
+    transition: {
+      duration: 0.7,
+      ease: "easeInOut",
+      yoyo: Infinity,
+    },
+  },
+};
+
 function Loading() {
   return (
     <LoadingContainer>
       <Text>Loading...</Text>
-      <Dots>
-        <Dot></Dot>
-        <Dot></Dot>
-        <Dot></Dot>
+      <Dots
+        as={motion.div}
+        variants={dotsContainer}
+        initial="initial"
+        animate="animate"
+      >
+        <Dot as={motion.span} variants={dotsVariants}></Dot>
+        <Dot as={motion.span} variants={dotsVariants}></Dot>
+        <Dot as={motion.span} variants={dotsVariants}></Dot>
       </Dots>
     </LoadingContainer>
   );
